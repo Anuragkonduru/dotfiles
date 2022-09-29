b8ddefb1cff90658a9dc1b208a624d520acb8a53
Shadow-Monarch98 <76196218+Shadow-Monarch98@users.noreply.github.com>
2022-09-30 05:29:14 +0530

    mpv configs

---
+++ config/mpv/scripts/toggle-shuffle.js
@@ -0,0 +1,10 @@
+var state = 0
+var commands = [
+    'playlist-shuffle',
+    'playlist-unshuffle'
+]
+function perform_action() {
+    mp.command(commands[state])
+    state = (state + 1) % commands.length
+}
+mp.add_key_binding('shift+r','toggle-shuffle', perform_action)
