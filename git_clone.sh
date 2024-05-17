c5d68c490bcfaa85b21c0e1e088019924912a2da
Anurag <konduruanurag@gmail.com>
2024-05-17 23:31:22 +0530

    gh clone script

---
+++ git_clone.sh
@@ -0,0 +1,16 @@
+gh repo clone Anuragkonduru/profile
+gh repo clone Anuragkonduru/Obsidian-Notes.git
+gh repo clone Anuragkonduru/dotfiles
+gh repo clone Anuragkonduru/ArchInstallScript
+gh repo clone Anuragkonduru/Coding
+gh repo clone Anuragkonduru/blog
+gh repo clone Anuragkonduru/Projects
+gh repo clone Anuragkonduru/DSVisualizer
+gh repo clone Anuragkonduru/RetireC
+gh repo clone Anuragkonduru/wallpapers
+gh repo list AnuragKonduru
+gh repo clone AnuragKonduru/*
+gh repo clone Anuragkonduru/bookdashy-firefox
+gh repo clone Anuragkonduru/tool
+gh repo clone Anuragkonduru/mylogos
+
