349701c71ae5bee33016e69626863dbf881f4d93
Anurag Konduru <konduruanurag@gmail.com>
2026-08-25 18:01:28 +0530

    initial keybinds for quattro

--- .config/hypr/bindings.lua
+++ .config/hypr/bindings.lua
@@ -27,3 +27,31 @@
 -- o.bind("SUPER + SHIFT + S", nil, "omarchy-capture-screenshot")
 -- o.bind("SUPER + H", nil, "voxtype record toggle")
 -- o.bind("SUPER + PERIOD", nil, "omarchy-shell shell toggle omarchy.emojis")
+-- Environemt Bindings
+hl.env("XCURSOR_THEME", "Bibata-Modern-Ice")
+hl.env("HYPRCURSOR_THEME", "Bibata-Modern-Ice")
+
+-- My Custom Bindings
+hl.unbind("SUPER + P")
+hl.unbind("SUPER + W")
+hl.unbind("SUPER + SHIFT + RETURN")
+hl.unbind("SUPER + SHIFT + C")
+
+o.bind("SUPER + D", "Apps Menu", "omarchy-menu toggle apps")
+o.bind("SUPER + Q", "Close window", hl.dsp.window.close())
+o.bind("SUPER + ALT + P", "Pseudo window", hl.dsp.window.pseudo())
+o.bind("SUPER + B", "Browser", { omarchy = "browser" })
+o.bind("SUPER + Z", "Zed Editor", "zed")
+o.bind("SUPER + CTRL + C", "Clipboard manager", "omarchy-shell shell toggle omarchy.clipboard")
+o.bind("SUPER + X", "Logout Menu", "omarchy-menu toggle system")
+o.bind("SUPER + E", "Files", "Thunar")
+o.bind("SUPER + M", "Files", "foot -e rmpc")
+
+
+for workspace = 1, 10 do
+  local key = "code:" .. tostring(workspace + 9)
+  hl.unbind("SUPER + SHIFT + " .. key)
+  hl.unbind("SUPER + SHIFT + ALT + " .. key)
+  o.bind("ALT + " .. key, "Move window to workspace " .. workspace, hl.dsp.window.move({ workspace = tostring(workspace) }))
+  o.bind("SUPER + SHIFT + " .. key, "Move window silently to workspace " .. workspace, hl.dsp.window.move({ workspace = tostring(workspace), follow = false }))
+end
